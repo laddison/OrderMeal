0bad0e8ed729b231a87703a1d58a60805abc5268
addisons <1043791113@qq.com>
2019-02-26 17:21:52 +0800

    init

--- model/user.go
+++ model/user.go
@@ -44,7 +44,7 @@ func (this *User) GetUser() map[int]string {
 func (this *User) GetMenu() map[int]string {
 	menu := make(map[int]string)
 	menu[1] = "咖喱鸡肉饭"
-	menu[2] = "茶菇鸭肉饭"
+	menu[2] = "梅菜扣肉饭"
 	menu[3] = "台式肥牛饭"
 	menu[4] = "阿嬷牛肉面"
 	menu[5] = "黑椒面"
